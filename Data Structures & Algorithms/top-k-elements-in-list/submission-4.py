49b50435ff2f83a622a4ba4bf81520b7210afbe4
Nicholas Wise <nicholasmwise@gmail.com>
2026-05-26 13:44:52 -0500

    Add: top-k-elements-in-list - submission-4

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # First I'll generate a dictionary that acts as a hashMap, we will store each unique number with it's frrequency
+        hashMap = {}
+        # For each number, check if it's a key, if so increment value, otherwise generate new key and set value to 1
+        for num in nums:
+            if num not in hashMap:
+                hashMap[num] = 1
+            else:
+                hashMap[num] += 1
+
+        # I now have a dictionary where each key is a unique number from the origianl list, and each value is it's frequncy in that list
+        
+        # Now I will find which entry has the highest frequency for as many times as k equals
+        output = []
+        i = 0
+        while i < k:
+            # We set the max to the first key in the dict
+            max = next(iter(hashMap))
+            # Now we check each entry in dictionary to find max
+            for num in hashMap:
+                if hashMap[num] > hashMap[max]:
+                    max = num
+            # Once we have the max, we add it to output, and remove it from the dict
+            output.append(max)
+            hashMap.pop(max)
+            i += 1
+        return output
+            
