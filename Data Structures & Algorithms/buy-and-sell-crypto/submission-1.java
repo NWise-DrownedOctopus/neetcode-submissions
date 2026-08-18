b25a84dee0dab2e9e0681388c195fad0873ad7bd
Nicholas Wise <nicholasmwise@gmail.com>
2026-08-18 15:34:40 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int[] profits = new int[prices.length];
+
+        // Ok so we need to find the best time to buy and sell. In other words max(prices[sell] - prices[buy])
+        // We can find the minimum, but that won't necissarially give us the highest delta between buy and sell
+        // How could we find the delta. We could maybe do something with two pointers.
+        int firstIndex = 0;
+        int secondIndex = 1;
+        int max = 0;
+        // ok so we start with the first index, we check ever other index for a profit, if that is better than the current entry, we update the value
+        // We keep doing this until we get to the end
+        while (firstIndex <= prices.length - 2) {
+            while(secondIndex <= prices.length - 1) {
+                int profit = prices[secondIndex] - prices[firstIndex];
+                if (profit > profits[firstIndex]) {
+                    profits[firstIndex] = profit;
+                    if (profit > max) {
+                        max = profit;
+                    }
+                    
+                }                
+                secondIndex += 1;
+            }
+
+            firstIndex += 1;
+            secondIndex = firstIndex + 1;            
+        }
+
+        return max;
+    }
+}
